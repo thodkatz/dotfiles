60ad6c11c47ead33df90ff471d0ee31244a53d6f
thodkatz <thodkatz@gmail.com>
2021-04-24 23:24:18 +0300

    Update soft links

--- link.sh
+++ link.sh
@@ -2,19 +2,24 @@
 # Fix me!
 
 var=~/repos/dotfiles
-echo "Success for the string concatenation $var"
+
+mkdir -p ~/.config/nvim/general
+mkdir -p ~/.config/nvim/keys
+mkdir -p ~/.config/nvim/vim-plug
+mkdir -p ~/.config/nvim/plug-conf
 
 # f flag to override/update in case it already exists
 # s for the soft link
 # v for the verbose, to print the link name
-ln -sfv $var/.zshrc ~/
-ln -sfv $var/.vimrc ~/
-ln -sfv $var/alacritty ~/.config/
-ln -sfv $var/.doom.d ~/
-ln -sfv $var/picom.conf ~/.config/
-ln -sfv $var/.xinitrc ~/
-ln -sfv $var/nvim/* ~/.config/nvim/
-ln -sfv $var/nvim/general/* ~/.config/nvim/general/
-ln -sfv $var/nvim/keys/* ~/.config/nvim/keys/
-ln -sfv $var/nvim/vim-plug/* ~/.config/nvim/vim-plug/
-ln -sfv $var/nvim/plug-conf/* ~/.config/nvim/plug-conf/
+ln -snfv $var/.zshrc ~/
+ln -snfv $var/.vimrc ~/
+ln -snfv $var/alacritty ~/.config/
+ln -snfv $var/.doom.d ~/
+ln -snfv $var/picom.conf ~/.config/
+ln -snfv $var/.xinitrc ~/
+ln -snfv $var/nvim/init.vim ~/.config/nvim/
+ln -sfnv $var/nvim/coc-setting.json ~/.config/nvim/
+ln -snfv $var/nvim/general/* ~/.config/nvim/general/
+ln -snfv $var/nvim/keys/* ~/.config/nvim/keys/
+ln -snfv $var/nvim/vim-plug/* ~/.config/nvim/vim-plug/
+ln -sfnv $var/nvim/plug-conf/* ~/.config/nvim/plug-conf/
